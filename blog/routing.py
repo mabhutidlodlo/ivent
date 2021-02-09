c1709cc5c720e88a086555aea610555acbc49aee
mabhutidlodlo <mabhutidlodlo1@gmail.com>
2021-02-09 16:12:55 +0200

    routes

--- blog/routing.py
+++ blog/routing.py
@@ -4,5 +4,5 @@ from django.urls import re_path
 from . import consumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/notify/(?P<id>[0-9]+)/(?P<token>\w+)/$', consumer.NotificationConsumer),
+    re_path(r'wss/notify/(?P<id>[0-9]+)/(?P<token>\w+)/$', consumer.NotificationConsumer),
 ]
