1d7bf6f10996bdedd74373b5dfa324f4bde9400e
mabhutidlodlo <30758403+mabhutidlodlo@users.noreply.github.com>
2021-02-11 03:09:24 +0200

    Update settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -102,7 +102,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "LOCATION": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
 
     },
