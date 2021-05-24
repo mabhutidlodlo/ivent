9082009683c160935c67c6bdc7c32835b6200bd0
mabhutidlodlo <mabhutidlodlo1@gmail.com>
2021-05-24 23:35:02 +0200

    mm

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5p#7m6+0eg^y#_$409gvbs@m19z251#0yy3r!iw!%)o-l0&xwk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
 '*'
